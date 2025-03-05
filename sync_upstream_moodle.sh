db69409835ed9a3e4e07572efb0cd8b2ae0db2c4
866042 <aitor.renobales@ehu.eus>
2025-03-05 12:12:51 +0100

    Revert "sync"
    This reverts commit bbd7973595e15316853a476ae87f5408640409ae.

--- sync_upstream_moodle.sh
+++ sync_upstream_moodle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git fetch upstream
-for BRANCH in MOODLE_405_STABLE; do
+for BRANCH in MOODLE_405_STABLE main; do
     git push origin refs/remotes/upstream/$BRANCH:refs/heads/$BRANCH
 done
