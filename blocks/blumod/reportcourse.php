2cd6079bde73273d60586e1fae218d87f41f416a
Aitor Renobales Irusta <aitor.renobales@ehu.eus>
2025-05-02 20:42:53 +0200

    paginar tabla

--- blocks/blumod/reportcourse.php
+++ blocks/blumod/reportcourse.php
@@ -31,6 +31,8 @@ require('../../config.php');
 require_once $CFG->dirroot.'/blocks/blumod/lib.php';
 require_once $CFG->dirroot.'/blocks/blumod/classes/reportcourse.php';
 require_once $CFG->libdir.'/tablelib.php';
+require_once $CFG->libdir . '/csvlib.class.php';
+
 
  define('DEFAULT_PAGE_SIZE', 20);
  define('SHOW_ALL_PAGE_SIZE', 5000);
@@ -40,6 +42,8 @@ global $DB, $CFG, $PAGE;
 
 $courseid = required_param('courseid', PARAM_INT);
 // $id       = required_param('id', PARAM_INT); // course id.
+$exportcsv = optional_param('exportcsv', false, PARAM_BOOL);
+
 $course = $DB->get_record('course', ['id'=>$courseid], '*', MUST_EXIST);
 $url = new moodle_url('/block/blumod/reportcourse.php', array(
     'courseid' => $course->id));
@@ -65,22 +69,40 @@ $pluginname = get_string('reportblumod', 'block_blumod');
 // Release session lock.
 \core\session\manager::write_close();
 
-// $reporttable = report_blumod_get_table_name(); // Log table to use for fetaching records.
-
 $baseurl = new moodle_url('/blocks/blumod/reportcourse.php', array(
     'courseid' => $course->id
 ));
+
+if ($exportcsv) {
+    $reportcourse = new reportcourse_selector($courseid);
+    $data_export = $reportcourse->loadData();
+    export_to_csv($data_export);
+    exit;
+}
+
+
+echo html_writer::start_tag('form', ['class' => 'form-inline', 'method' => 'get', 'action' => $url]);
+echo html_writer::empty_tag('input', ['type' => 'hidden', 'name' => 'courseid', 'value' => $courseid]);
+echo html_writer::empty_tag('input', ['type' => 'submit', 'name' => 'exportcsv', 'value' => get_string('exportcsv', 'block_blumod'), 'class' => 'btn btn-secondary']);
+echo html_writer::end_tag('form');
+
 $select = groups_allgroups_course_menu($course, $baseurl, true, $currentgroup);
+
+$data = $reportcourse->loadData();
+$number_of_records = count($data);
 
 $table = new flexible_table('blumod-report-'.$course->id);
 $table->course = $course;
+
+$table->initialbars(true);
 
 $table->define_columns(array('bluid', 'bluname', 'blumodid', 'blumodmodule', 'cmmodule', 'cminstance', 'resource_name', 'competencyid', 'competencyshortname'));
 $table->define_headers(array('BLUID', 'BLU', 'BLUMODID', 'BLUMODMODULE', 'CMMODULE', 'CMINSTANCE', 'RESOURCE_NAME', 'COMPETENCYID', 'COMPETENCY'));
 $table->define_baseurl($baseurl);
 $table->set_attribute('class', 'generaltable generalbox reporttable');
-$table->sortable(true, 'lastname', SORT_ASC);
-$table->no_sorting('select');
+$table->sortable(true, 'bluname', SORT_ASC);
+$table->pagesize(DEFAULT_PAGE_SIZE, $number_of_records);
+
 $table->set_control_variables(array(
                                 TABLE_VAR_SORT    => 'ssort',
                                 TABLE_VAR_HIDE    => 'shide',
@@ -89,13 +111,14 @@ $table->set_control_variables(array(
                                 TABLE_VAR_ILAST   => 'silast',
                                 TABLE_VAR_PAGE    => 'spage',
                                 ));
+$page_table = optional_param('spage', 0, PARAM_INT);
+                                
 $table->setup();
 
-// $table->initialbars($totalcount > $perpage);
-// $table->pagesize($perpage, $matchcount);
+$start_table = $page_table * DEFAULT_PAGE_SIZE;
+$end_table = $start_table + DEFAULT_PAGE_SIZE;
 
-$data = $reportcourse->loadData();
-foreach ($data as $row) {
+foreach (array_slice($data, $start_table, DEFAULT_PAGE_SIZE) as $row) {
     $table->add_data($row);
 }
 $table->finish_output();
@@ -112,3 +135,30 @@ echo '<input type="submit" value="'.get_string('go').'" class="btn btn-primary"/
 
 
 echo $OUTPUT->footer();
+
+function export_to_csv($data) {
+    $csvwriter = new csv_export_writer();
+    $csvwriter->set_filename('blumod_report_' . date('Ymd_His'));
+
+    // Add headers.
+    $csvwriter->add_data([
+        get_string('bluid', 'block_blumod'),
+        get_string('bluname', 'block_blumod'),
+        get_string('blumodid', 'block_blumod'),
+        get_string('blumodmodule', 'block_blumod'),
+        get_string('cmmodule', 'block_blumod'),
+        get_string('cminstance', 'block_blumod'),
+        get_string('resourcename', 'block_blumod'),
+        get_string('competencyid', 'block_blumod'),
+        get_string('competencyshortname', 'block_blumod')
+    ]);
+
+    // Add data rows.
+    foreach ($data as $row) {
+        $csvwriter->add_data($row);
+    }
+
+    // Download the CSV file.
+    $csvwriter->download_file();
+    exit;
+}
