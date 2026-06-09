e3fcef3a5c49b67494215d138e2a0b6e2cdd443e
Aitor Renobales Irusta <aitor.renobales@ehu.eus>
2026-06-09 12:43:50 +0200

    last changes

--- sync_upstream_moodle.sh
+++ sync_upstream_moodle.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
 # Esta variable es para guardar la rama antigua y recuperar archivos necesarios
-export BRANCH_OLD="galan_dev_and_deploy"
+export BRANCH_BACKUP="galan_backup"
 
-# Esta variable contiene el nombre de la rama segun la version de Moodle a la que queramos actualizar
+# Esta variable contiene el nombre de la rama segun la version major de Moodle a la que queramos actualizar, por ejemplo MOODLE_502_STABLE
 export BRANCH=MOODLE_502_STABLE
 
+# Verificar que estamos en la rama de la versión que vamos a actualizar, por ejemplo MOODLE_501_STABLE
+git branch --show-current
+
+# Llevar los cambios de la rama main a $BRANCH_BACKUP
+git branch -f $BRANCH_BACKUP
+
+# Actualizar desde el repo original de Moodle los últimos cambios 
 git fetch upstream
+
+# Crear la rama nueva a partir de la rama del upstream
 git checkout -b $BRANCH upstream/$BRANCH
-# git checkout $BRANCH_OLD -- sync_upstream_moodle.sh
-# git checkout $BRANCH_OLD -- blocks/blumod
 
-# Plugins Galan a recuperar
-for PLUGIN in sync_upstream_moodle.sh public/blocks/blumod; do
-  # git checkout $BRANCH_OLD -- $PLUGIN # Legacy
-  git restore --source=$BRANCH_OLD $PLUGIN
+# Plugins Galan a incorporar a la nueva rama $BRANCH
+for PLUGIN in sync_upstream_moodle.sh public/blocks/blumod README_Galan.md; do
+  # git checkout $BRANCH_BACKUP -- $PLUGIN # Legacy
+  git restore --source=$BRANCH_BACKUP $PLUGIN
 done
 
-# Otros plugins instalados que no están en el core
+# Otros Plugins a incorporar a la nueva rama $BRANCH
 for PLUGIN in public/blocks/configurable_reports public/blocks/people public/blocks/taggedcoursesearch public/mod/pdfannotator public/mod/quiz/accessrule/onesession public/mod/wooclap public/theme/moove; do
-  # git checkout $BRANCH_OLD -- $PLUGIN # Legacy
-  git restore --source=$BRANCH_OLD $PLUGIN
+  # git checkout $BRANCH_BACKUP -- $PLUGIN # Legacy
+  git restore --source=$BRANCH_BACKUP $PLUGIN
 done
 
+# Commit de los cambios y push a la nueva rama $BRANCH
 git status
 git add .
 git commit -m "sync, blumod, otros plugins $BRANCH"
 git push origin $BRANCH
+
+# Poner al día main con los cambios de $BRANCH
+git checkout main
+git merge -X theirs $BRANCH
+git push origin main
 
 # for BRANCH in MOODLE_500_STABLE; do
 #     git checkout -b $BRANCH upstream/$BRANCH
